c85355b54878c699f6bf0c25931a4cdd61802a89
KRFH <barcaisthebest0201@gmail.com>
2023-05-29 11:39:31 +0900

    set testsize randomstate at const.py

--- const.py
+++ const.py
@@ -3,3 +3,6 @@ import os
 INPUT_DIR = os.getcwd() + "/data/input_data"
 OUTPUT_DIR = os.getcwd() + "/data/output_data"
 API_PATH = "/Users/kai/Desktop/api/openai.txt"
+
+TEST_SIZE = 0.25
+RANDOM_STATE = 3655
